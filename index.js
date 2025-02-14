b57e119055bf37ba79c7c92ae1283ce7471da479
Obaid Web Developer <mobaidwebdeveloper@gmail.com>
2025-02-14 08:32:33 +0000

    op

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const port = 3000;;
 const path = require('path');
 const routes = require('./routes');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/database', express.static(path.join(__dirname, 'database')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/',routes);
 
-app.listen(port,() => {
-    console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port,() => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
+module.exports = app;
